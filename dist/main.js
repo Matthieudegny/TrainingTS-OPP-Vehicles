88ab3c7df55a8b3b13b68e9c52ce4fbbfe3a9e49
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-09-26 20:08:40 +0200

    feature rent vehicle done

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ var TypeVehicule;
     TypeVehicule["BUS"] = "Bus";
     TypeVehicule["VOITURE"] = "Voiture";
 })(TypeVehicule || (TypeVehicule = {}));
+//abstract peut etre utilisé uniquement entant que classe mère
 class Vehicule {
     constructor(_immatriculation, _type) {
         this._immatriculation = _immatriculation;
@@ -41,6 +42,9 @@ class ListeVehicules {
             this._liste.pop();
         }
     }
+    louerVehicule(vehicule) {
+        this._liste.splice(this._liste.indexOf(vehicule), 1);
+    }
 }
 class ParAuto {
     constructor() {
@@ -53,50 +57,83 @@ class ParAuto {
     get bus() {
         return this._bus;
     }
+    //extends j'attends un objet qui comprend la class véhicule
     ajouterVehicule(vehicule) {
         if (vehicule.type === TypeVehicule.VOITURE) {
-            this._voitures.ajouterVehicule(vehicule);
+            //rappel voitures est une instance de ListeVehicules avec les méthodes ajouterVehicule + retirerVehicule + louerVehicule
+            this.voitures.ajouterVehicule(vehicule);
         }
         else if (vehicule.type === TypeVehicule.BUS) {
-            this._bus.ajouterVehicule(vehicule);
+            this.bus.ajouterVehicule(vehicule);
         }
     }
     louerVehicule(type) {
         if (type === TypeVehicule.BUS) {
-            this._bus.retirerVehicule();
+            this.bus.retirerVehicule();
         }
         else if (type === TypeVehicule.VOITURE) {
-            this._voitures.retirerVehicule();
+            this.voitures.retirerVehicule();
         }
     }
     afficherParc() {
         console.log("Liste des voitures :");
-        for (let v of this._voitures.liste) {
+        for (let v of this.voitures.liste) {
             console.log("Immatriculation : " + v.immatriculation);
         }
         console.log("Liste des Bus :");
-        for (let b of this._bus.liste) {
+        for (let b of this.bus.liste) {
             console.log("Immatriculation : " + b.immatriculation);
         }
     }
     getVehicules() {
         let tab = [];
-        tab.push(...this._bus.liste, ...this._voitures.liste);
+        tab.push(...this.bus.liste, ...this.voitures.liste);
         return tab;
+    }
+    louerVehiculeImmat(immat) {
+        let vehicule = this.getVehiculeImmat(immat); //vehicule ou null
+        if (vehicule) {
+            if (vehicule.type === TypeVehicule.BUS) {
+                this.bus.louerVehicule(vehicule);
+            }
+            else if (vehicule.type === TypeVehicule.VOITURE) {
+                this.voitures.louerVehicule(vehicule);
+            }
+        }
+        else {
+            throw { message: "Erreur d'immatriculation" };
+        }
+    }
+    getVehiculeImmat(immat) {
+        let vehicules = this.getVehicules(); //tableau de tous les véhicules du parc
+        for (let v of vehicules) {
+            if (v.immatriculation === immat) {
+                return v;
+            }
+        }
+        return null;
     }
 }
 let parcMGA = new ParAuto();
 parcMGA.ajouterVehicule(new Bus("XX1111XX"));
 parcMGA.ajouterVehicule(new Bus("YY2222YY"));
+parcMGA.ajouterVehicule(new Bus("ZZ3333ZZ"));
 parcMGA.ajouterVehicule(new Voiture("AB1111CD"));
 parcMGA.ajouterVehicule(new Voiture("EF2222GH"));
-console.log(parcMGA.getVehicules());
-const creerListeVehiculeSelect = () => {
+parcMGA.ajouterVehicule(new Voiture("IJ3333KL"));
+// console.log(parcMGA.getVehicules());
+const selectListe = document.querySelector("#listeVehicule");
+selectListe.innerHTML = creerListeVehiculeSelect();
+const boutonLouer = document.querySelector("#louer");
+boutonLouer.addEventListener("click", () => {
+    const immat = selectListe.value;
+    parcMGA.louerVehiculeImmat(immat);
+    selectListe.innerHTML = creerListeVehiculeSelect();
+});
+function creerListeVehiculeSelect() {
     let txt = "";
     for (let v of parcMGA.getVehicules()) {
         txt += `<option value="${v.immatriculation}">${v.type} : ${v.immatriculation}</option>`;
     }
     return txt;
-};
-const selectListe = document.querySelector("#listeVehicule");
-selectListe.innerHTML = creerListeVehiculeSelect();
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ enum TypeVehicule {
   VOITURE = "Voiture",
 }
 
+//abstract peut etre utilisé uniquement entant que classe mère
 abstract class Vehicule {
   constructor(private _immatriculation: string, private _type: string) {}
   get immatriculation() {
@@ -39,6 +40,9 @@ class ListeVehicules<T> {
       this._liste.pop();
     }
   }
+  louerVehicule(vehicule: T) {
+    this._liste.splice(this._liste.indexOf(vehicule), 1);
+  }
 }
 
 class ParAuto {
@@ -52,53 +56,87 @@ class ParAuto {
     return this._bus;
   }
 
+  //extends j'attends un objet qui comprend la class véhicule
   ajouterVehicule<T extends Vehicule>(vehicule: T) {
     if (vehicule.type === TypeVehicule.VOITURE) {
-      this._voitures.ajouterVehicule(vehicule);
+      //rappel voitures est une instance de ListeVehicules avec les méthodes ajouterVehicule + retirerVehicule + louerVehicule
+      this.voitures.ajouterVehicule(vehicule);
     } else if (vehicule.type === TypeVehicule.BUS) {
-      this._bus.ajouterVehicule(vehicule);
+      this.bus.ajouterVehicule(vehicule);
     }
   }
   louerVehicule(type: TypeVehicule) {
     if (type === TypeVehicule.BUS) {
-      this._bus.retirerVehicule();
+      this.bus.retirerVehicule();
     } else if (type === TypeVehicule.VOITURE) {
-      this._voitures.retirerVehicule();
+      this.voitures.retirerVehicule();
     }
   }
   afficherParc() {
     console.log("Liste des voitures :");
-    for (let v of this._voitures.liste) {
+    for (let v of this.voitures.liste) {
       console.log("Immatriculation : " + v.immatriculation);
     }
     console.log("Liste des Bus :");
-    for (let b of this._bus.liste) {
+    for (let b of this.bus.liste) {
       console.log("Immatriculation : " + b.immatriculation);
     }
   }
 
   getVehicules(): Vehicule[] {
     let tab: Vehicule[] = [];
-    tab.push(...this._bus.liste, ...this._voitures.liste);
+    tab.push(...this.bus.liste, ...this.voitures.liste);
     return tab;
+  }
+
+  louerVehiculeImmat(immat: string) {
+    let vehicule = this.getVehiculeImmat(immat); //vehicule ou null
+    if (vehicule) {
+      if (vehicule.type === TypeVehicule.BUS) {
+        this.bus.louerVehicule(vehicule);
+      } else if (vehicule.type === TypeVehicule.VOITURE) {
+        this.voitures.louerVehicule(vehicule);
+      }
+    } else {
+      throw { message: "Erreur d'immatriculation" };
+    }
+  }
+
+  private getVehiculeImmat(immat: string): Vehicule | null {
+    let vehicules = this.getVehicules(); //tableau de tous les véhicules du parc
+    for (let v of vehicules) {
+      if (v.immatriculation === immat) {
+        return v;
+      }
+    }
+    return null;
   }
 }
 
 let parcMGA = new ParAuto();
 parcMGA.ajouterVehicule(new Bus("XX1111XX"));
 parcMGA.ajouterVehicule(new Bus("YY2222YY"));
+parcMGA.ajouterVehicule(new Bus("ZZ3333ZZ"));
 parcMGA.ajouterVehicule(new Voiture("AB1111CD"));
 parcMGA.ajouterVehicule(new Voiture("EF2222GH"));
+parcMGA.ajouterVehicule(new Voiture("IJ3333KL"));
 
-console.log(parcMGA.getVehicules());
+// console.log(parcMGA.getVehicules());
 
-const creerListeVehiculeSelect = (): string => {
+const selectListe = document.querySelector("#listeVehicule")! as HTMLSelectElement;
+selectListe.innerHTML = creerListeVehiculeSelect();
+
+const boutonLouer = document.querySelector("#louer")! as HTMLButtonElement;
+boutonLouer.addEventListener("click", () => {
+  const immat = selectListe.value;
+  parcMGA.louerVehiculeImmat(immat);
+  selectListe.innerHTML = creerListeVehiculeSelect();
+});
+
+function creerListeVehiculeSelect(): string {
   let txt = "";
   for (let v of parcMGA.getVehicules()) {
     txt += `<option value="${v.immatriculation}">${v.type} : ${v.immatriculation}</option>`;
   }
   return txt;
-};
-
-const selectListe = document.querySelector("#listeVehicule")! as HTMLSelectElement;
-selectListe.innerHTML = creerListeVehiculeSelect();
+}
